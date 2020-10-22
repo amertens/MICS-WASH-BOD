b7365c0c17d988d6e376b5c480c54b89f78f9086
andrew.mertens <andrew.mertens3@gmail.com>
2020-10-21 20:21:00 -0700

    rerun multi

--- src/03-POC-analysis-multinomial.R
+++ src/03-POC-analysis-multinomial.R
@@ -1,21 +1,5 @@
 
 
-#NOTES
-# What is the proper weighting of the regression functions?
-# What is the proper WASH-less asset index to use?
-# Is roof/floor/wall material and number of rooms already in the wealth index?
-  
-#Need to move BH to main folders
-#need to check all cleaned surveys are mics6 and there are no MICS5
-#Need to see if bh.sav is available online for downloaded surveys missing it
-
-#BH datasets are much longer than other datasets... 
-#need to check if longitudinal and I'm merging correctly. I'm just using birth order
-
-#left_joining bh is increasing dataset size
-#NOTE: need to look up line number and double check that I am merging datasets correctly
-
-
 
 source("0-config.R")
 
@@ -62,7 +46,7 @@ d$clust_num <- paste0(d$clust_num, "-",d$HH_num)
 # res <- mics_multinomial_regression(d=d,
 #                 Y ="stunt",
 #                 X="EC_risk_H",
-#                 W=NULL,
+#                 W=Wvars,
 #                 weight = "ecpopweight_H",
 #                 clustid= "clust_num",
 #                 family="modified possion", calc_PAF=F, low_risk_level=1)
