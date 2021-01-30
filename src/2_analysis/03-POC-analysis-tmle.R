8a3d63d6c4a3dcbfa5fbb926d04ed1333f351827
andrew.mertens <andrew.mertens3@gmail.com>
2021-01-30 12:33:49 -0800

    run tmle

--- src/2_analysis/03-POC-analysis-tmle.R
+++ src/2_analysis/03-POC-analysis-tmle.R
@@ -23,7 +23,10 @@ res_adj <- res_adj_bin <- res_adj_cont <- NULL
 
 d <- droplevels(d)
 res_adj_bin <- run_mics_tmle(outcomes = c("diarrhea"), family="binomial",  Wvars=Wvars)
+saveRDS(res_adj_bin, here("results/adjusted_tmle_ests_bin.rds"))
+
 res_adj_cont <- run_mics_tmle(outcomes = c("haz"), family="gaussian", Wvars=Wvars)
+saveRDS(res_adj_cont, here("results/adjusted_tmle_ests_cont.rds"))
 
 
 
