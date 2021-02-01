30848793f80be0e00c1785ac2c9713243f8d2eac
andrew.mertens <andrew.mertens3@gmail.com>
2021-01-31 22:20:48 -0800

    update

--- src/2_analysis/02-primary-PAF.R
+++ src/2_analysis/02-primary-PAF.R
@@ -8,9 +8,9 @@ source("0-config.R")
 #load clean data
 d <- readRDS(here("data/compiled_clean_MICS_survey.rds"))
 
-
-res_unadj_bin <- run_MICS_regressions(outcomes = c("stunt", "wast","diarrhea","ari"), family="modified possion", PAF=T, Wvars=NULL)
-res_adj_bin <- run_MICS_regressions(outcomes = c("stunt", "wast","diarrhea","ari"), family="modified possion", PAF=T, Wvars=Wvars)
+res_unadj_bin <- NULL
+#res_unadj_bin <- run_MICS_regressions(outcomes = c("stunt","diarrhea"), family="modified possion", PAF=T, Wvars=NULL)
+res_adj_bin <- run_MICS_regressions(outcomes = c("stunt","diarrhea"), family="modified possion", PAF=T, Wvars=Wvars)
 res_unadj_bin <- res_unadj_bin %>% mutate(adjusted=0)
 res_adj_bin <- res_adj_bin %>% mutate(adjusted=1)
 
