5a0289a0a3a3bcee6ef6dca2fc7a883255bb8001
andrew.mertens <andrew.mertens3@gmail.com>
2021-02-04 07:45:35 -0800

    rerun

--- src/2_analysis/02-pooled-PAF.R
+++ src/2_analysis/02-pooled-PAF.R
@@ -12,7 +12,6 @@ d$country2 <- factor(d$country)
 d$country <- "pooled"
 Wvars <- c(Wvars, "country2")
 
-res_unadj_bin <- NULL
 res_adj_bin <- run_MICS_regressions(outcomes = c("stunt","diarrhea"), family="modified possion", PAF=T, Wvars=Wvars)
 res_adj_bin <- res_adj_bin %>% mutate(adjusted=1)
 
