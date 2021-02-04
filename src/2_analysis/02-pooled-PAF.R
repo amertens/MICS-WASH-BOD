de0c8b70b031c4ae8b1d18cb0c9e0b05034bd6a6
andrew.mertens <andrew.mertens3@gmail.com>
2021-02-03 22:38:43 -0800

    update

---
+++ src/2_analysis/02-pooled-PAF.R
@@ -0,0 +1,23 @@
+
+
+rm(list=ls())
+source("0-config.R")
+
+
+
+#load clean data
+d <- readRDS(here("data/compiled_clean_MICS_survey.rds"))
+
+d$country2 <- factor(d$country)
+d$country <- "pooled"
+Wvars <- c(Wvars, "country2")
+
+res_unadj_bin <- NULL
+res_adj_bin <- run_MICS_regressions(outcomes = c("stunt","diarrhea"), family="modified possion", PAF=T, Wvars=Wvars)
+res_adj_bin <- res_adj_bin %>% mutate(adjusted=1)
+
+
+
+
+saveRDS(res_adj_bin, here("results/pooled_PAFs.rds"))
+
